ff792204fa088b540dc5b462ff7924f2113c5b56
Pochyxi <adienerlopez@gmail.com>
2024-10-22 11:03:11 +0200

    Ottimizzazione EmailServiceImpl

--- src/main/java/com/adi/gestuser/service/impl/EmailServiceImpl.java
+++ src/main/java/com/adi/gestuser/service/impl/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.adi.gestuser.service.impl;
 
 import com.adi.gestuser.service.EmailService;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -18,7 +19,7 @@ import java.nio.charset.StandardCharsets;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    private static final Logger logger = LoggerFactory.getLogger( EmailServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger( EmailServiceImpl.class );
 
     @Value("${spring.mail.verify.host}")
     private String host;
@@ -28,119 +29,98 @@ public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender javaMailSender;
 
-
-    /**
-     * SEND MAIL MESSAGE
-     * @param name nome dell'utente
-     * @param to email dell'utente
-     * @param token token di verifica
-     * @param temporaryPassword password temporanea
-     * @param subject oggetto della mail
-     */
-    @Override
-    @Async
-    public void sendMailMessage(String name, String to, String token, String temporaryPassword, String subject) {
-
+    private void sendGenericMailMessage( String to, String subject, String html ) {
         try {
 
             // Crea un nuovo messaggio di posta elettronica.
             MimeMessage message = javaMailSender.createMimeMessage();
             // Crea un nuovo helper per il messaggio di posta elettronica.
-            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
-
-            // Ottieni il codice HTML per la mail di verifica.
-            String html = getHtmlVerify(name, token, temporaryPassword);
+            MimeMessageHelper helper = new MimeMessageHelper( message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name() );
 
             // Imposta il soggetto, il mittente, il destinatario e il testo del messaggio.
-            helper.setSubject(subject);
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setText(html, true);
+            helper.setSubject( subject );
+            helper.setFrom( fromEmail );
+            helper.setTo( to );
+            helper.setText( html, true );
 
-            // Aggiungi l'immagine come risorsa inline.
-            ClassPathResource logoImage = new ClassPathResource("static/images/logo.jpg");
-            helper.addInline("logoImage", logoImage);
+            declareLogoImage( helper );
 
             // Invia il messaggio di posta elettronica.
-            javaMailSender.send(message);
-        } catch (Exception e) {
-            logger.error( "Errore nell'invio della mail di verifica: {}", e.getMessage() );
+            javaMailSender.send( message );
+        } catch( Exception e ) {
+            logger.error( "Errore nell'invio della mail: {}", e.getMessage() );
         }
+    }
+
+
+    /**
+     * SEND MAIL MESSAGE
+     *
+     * @param name              nome dell'utente
+     * @param to                email dell'utente
+     * @param token             token di verifica
+     * @param temporaryPassword password temporanea
+     * @param subject           oggetto della mail
+     */
+    @Override
+    @Async
+    public void sendMailMessage( String name, String to, String token, String temporaryPassword, String subject ) {
+
+        String html = getHtmlVerify( name, token, temporaryPassword );
+        sendGenericMailMessage( to, subject, html );
     }
 
     /**
      * RESEND MAIL MESSAGE
-     * @param name nome dell'utente
-     * @param to email dell'utente
-     * @param token token di verifica
+     *
+     * @param name              nome dell'utente
+     * @param to                email dell'utente
+     * @param token             token di verifica
      * @param temporaryPassword password temporanea
-     * @param subject oggetto della mail
+     * @param subject           oggetto della mail
      */
     @Override
     @Async
     public void resendMailMessage( String name, String to, String token, String temporaryPassword, String subject ) {
-        try {
+        // Ottieni il codice HTML per la mail di verifica.
+        String html = getResendEmailVerify( name, token, temporaryPassword );
 
-            // Crea un nuovo messaggio di posta elettronica.
-            MimeMessage message = javaMailSender.createMimeMessage();
-            // Crea un nuovo helper per il messaggio di posta elettronica.
-            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+        // Invia la mail.
+        sendGenericMailMessage( to, subject, html );
 
-            // Ottieni il codice HTML per la mail di verifica.
-            String html = getResendEmailVerify(name, token, temporaryPassword);
-
-            // Imposta il soggetto, il mittente, il destinatario e il testo del messaggio.
-            helper.setSubject(subject);
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setText(html, true);
-
-            // Invia il messaggio di posta elettronica.
-            javaMailSender.send(message);
-        } catch (Exception e) {
-            logger.error( "Errore nell'invio della mail di verifica ripristino password: {}", e.getMessage() );
-        }
     }
 
 
     /**
      * SEND RECOVERY MESSAGE
-     * @param name nome dell'utente
-     * @param to email dell'utente
+     *
+     * @param name  nome dell'utente
+     * @param to    email dell'utente
      * @param token token di verifica
      */
     @Override
     @Async
-    public void sendRecoveryMessage(String name, String to, String token) {
-        try {
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+    public void sendRecoveryMessage( String name, String to, String token ) {
 
-            String html = getHtmlRecovery(name, token);
+        String html = getHtmlRecovery( name, token );
+        sendGenericMailMessage( to, "Recupero Password", html );
 
-            helper.setSubject("Richiesta di cambio password");
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setText(html, true);
-
-            javaMailSender.send(message);
-        } catch (Exception e) {
-            logger.error( "Errore nell'invio della mail di recupero password: {}", e.getMessage() );
-        }
     }
 
 
     /**
      * GET HTML RECOVERY
-     * @param name nome utente
+     *
+     * @param name  nome utente
      * @param token token di verifica
      * @return codice HTML per la mail di recupero password
      */
-    private String getHtmlRecovery(String name, String token){
+    private String getHtmlRecovery( String name, String token ) {
         String verificationUrl = host + "auth/change-password?token=" + token;
 
         return "<html>" +
                 "<body>" +
+                "<img src='cid:logoImage' alt='SmartAxcy Logo' style='width: 200px;'/>" +
                 "<h1>Ciao " + name + ",</h1>" +
                 "<p>Hai richiesto un cambio password</p>" +
                 "<br />" +
@@ -158,22 +138,23 @@ public class EmailServiceImpl implements EmailService {
 
     /**
      * GET HTML VERIFY
-     * @param name nome utente
-     * @param token token di verifica
+     *
+     * @param name              nome utente
+     * @param token             token di verifica
      * @param temporaryPassword password temporanea
      * @return codice HTML per la mail di verifica
      */
-    public String getHtmlVerify(String name, String token, String temporaryPassword) {
+    public String getHtmlVerify( String name, String token, String temporaryPassword ) {
         String verificationUrl = host + "auth/email-confirmation?token=" + token;
 
         return "<html>" +
                 "<body>" +
+                "<img src='cid:logoImage' alt='SmartAxcy Logo' style='width: 200px;'/>" +
                 "<h1>Ciao " + name + ",</h1>" +
                 "<p>Benvenuto in SmartAxcy, di seguito la tua Password temporanea: </p>" +
                 "<br />" +
                 "<p>" + temporaryPassword + "</p>" +
                 "<br />" +
-                "<img src='cid:logoImage' alt='SmartAxcy Logo' style='width: 200px;'/>" +
                 "<p>Al primo Login ti verrà chiesto di cambiare la Password, ma prima abbiamo bisogno che tu verifichi questa email.</p>" +
                 "<p>Per favore clicca sul bottone sottostante per verificare il tuo account:</p>" +
                 "<br />" +
@@ -189,16 +170,18 @@ public class EmailServiceImpl implements EmailService {
 
     /**
      * GET RESEND EMAIL VERIFY
-     * @param name nome utente
-     * @param token token di verifica
+     *
+     * @param name              nome utente
+     * @param token             token di verifica
      * @param temporaryPassword password temporanea
      * @return codice HTML per la mail di verifica
      */
-    public String getResendEmailVerify(String name, String token, String temporaryPassword) {
+    public String getResendEmailVerify( String name, String token, String temporaryPassword ) {
         String verificationUrl = host + "auth/email-confirmation?token=" + token;
 
         return "<html>" +
                 "<body>" +
+                "<img src='cid:logoImage' alt='SmartAxcy Logo' style='width: 200px;'/>" +
                 "<h1>Ciao " + name + ",</h1>" +
                 "<p>Ciao! La password del tuo account è stata ripristinata, di seguito le nuove informazioni. </p>" +
                 "<br />" +
@@ -217,5 +200,10 @@ public class EmailServiceImpl implements EmailService {
                 "</html>";
     }
 
+    private void declareLogoImage( MimeMessageHelper helper ) throws MessagingException {
+        // Aggiungi l'immagine come risorsa inline.
+        ClassPathResource logoImage = new ClassPathResource( "static/images/" + "logo.jpg" );
+        helper.addInline( "logoImage", logoImage );
+    }
 
 }
